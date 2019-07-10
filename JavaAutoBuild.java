3014afed24e5f687dabf857b069a004eced6185b
Mehmet Erkan <Mehmet-Erkan@users.noreply.github.com>
2019-07-10 22:18:34 +0200

    Delete JavaAutoBuild.java

--- JavaAutoBuild.java
+++
@@ -1,7 +0,0 @@
-public class JavaAutoBuild {
-
-  public static void main(String[] args) {
-    System.out.println("Hallo Docker Workshop!");
-    System.out.println("Ich wurde am Docker Donnerstag automatisch erstellt!");
-  }
-}
